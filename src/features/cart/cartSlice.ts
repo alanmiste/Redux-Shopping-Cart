576cf01579e2ac7fcf0f66fb842966051ff55656
Alan Miste <baranalaan@gmail.com>
2022-11-24 21:03:04 +0100

    Add addToCart method in reducer.

--- src/features/cart/cartSlice.ts
+++ src/features/cart/cartSlice.ts
@@ -1,4 +1,4 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
 export interface CartState {
     item: { [productID: string]: number }
@@ -11,7 +11,17 @@ const initialState: CartState = {
 const cartSlice = createSlice({
     name: "cart",
     initialState,
-    reducers: {}
+    reducers: {
+        addToCart(state, action: PayloadAction<string>){
+            const id = action.payload;
+            if(state.item[id]){
+                state.item[id]++
+            } else{
+                state.item[id] = 1
+            }
+        }
+    }
 });
 
+export const { addToCart } = cartSlice.actions;
 export default cartSlice.reducer;
