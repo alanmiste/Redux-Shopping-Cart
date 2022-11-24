c0f227c53c1690e08538fc172f5150679bf9693a
Alan Miste <baranalaan@gmail.com>
2022-11-24 21:06:14 +0100

    Add onClick to "Add to Cart" butoon.

--- src/features/products/Products.tsx
+++ src/features/products/Products.tsx
@@ -3,6 +3,7 @@ import { getProducts, Product } from "../../app/api";
 import { useAppSelector, useAppDispatch } from "../../app/hooks";
 import styles from "./Products.module.css";
 import { receivedProducts } from "./productSlice";
+import { addToCart } from "../cart/cartSlice";
 
 export function Products() {
   const dispatch = useAppDispatch();
@@ -30,7 +31,7 @@ export function Products() {
                 <h1>{product.name}</h1>
                 <p>{product.description}</p>
                 <p>${product.price}</p>
-                <button>Add to Cart 🛒</button>
+                <button onClick={()=>dispatch(addToCart(product.id))}>Add to Cart 🛒</button>
               </div>
             </article>
           </li>
