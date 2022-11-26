48a4be1af29562c94b4b0a01b53668c531eaf4eb
Alan Miste <baranalaan@gmail.com>
2022-11-26 17:56:17 +0100

    Add onQuantityChanged function.

--- src/features/cart/Cart.tsx
+++ src/features/cart/Cart.tsx
@@ -1,13 +1,18 @@
 import React from "react";
 import { useAppSelector, useAppDispatch } from "../../app/hooks";
 import styles from "./Cart.module.css";
-import { getTotalPrice, removeFromCart } from "./cartSlice";
+import { getTotalPrice, removeFromCart, updateQuantity } from "./cartSlice";
 
 export function Cart() {
-  const products = useAppSelector((state)=> state.products.products);
-  const items = useAppSelector((state)=> state.cart.items);
+  const products = useAppSelector((state) => state.products.products);
+  const items = useAppSelector((state) => state.cart.items);
   const totalPrice = useAppSelector(getTotalPrice);
   const dispatch = useAppDispatch();
+
+  function onQuantityChanged(e: React.FocusEvent<HTMLInputElement>, id: string) {
+    const quantity = Number(e.target.value) || 0;
+    dispatch(updateQuantity({ id, quantity }));
+  }
   return (
     <main className="page">
       <h1>Shopping Cart</h1>
@@ -21,21 +26,25 @@ export function Cart() {
           </tr>
         </thead>
         <tbody>
-          {Object.entries(items).map(([id, quantity])=>(
-             <tr key={id}>
-            <td>{products[id].name}</td>
-            <td>
-              <input type="text" className={styles.input} defaultValue={quantity} />
-            </td>
-            <td>{products[id].price}</td>
-            <td>
-              <button 
-              aria-label={`Remove ${products[id].name} from Shopping Cart`}
-              onClick={()=> dispatch(removeFromCart(id))}>
-                X
-              </button>
-            </td>
-          </tr>
+          {Object.entries(items).map(([id, quantity]) => (
+            <tr key={id}>
+              <td>{products[id].name}</td>
+              <td>
+                <input
+                  type="text"
+                  className={styles.input}
+                  defaultValue={quantity}
+                  onBlur={(e) => onQuantityChanged(e, id)} />
+              </td>
+              <td>{products[id].price}</td>
+              <td>
+                <button
+                  aria-label={`Remove ${products[id].name} from Shopping Cart`}
+                  onClick={() => dispatch(removeFromCart(id))}>
+                  X
+                </button>
+              </td>
+            </tr>
           ))}
         </tbody>
         <tfoot>
