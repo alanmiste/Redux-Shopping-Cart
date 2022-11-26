d551d9c22abfd6a3a02bf1375beff67b6035d0a7
Alan Miste <baranalaan@gmail.com>
2022-11-26 14:41:05 +0100

    Add removeFromCart method in reducers.

--- src/features/cart/cartSlice.ts
+++ src/features/cart/cartSlice.ts
@@ -2,58 +2,61 @@ import { createSelector, createSlice, PayloadAction } from "@reduxjs/toolkit";
 import { RootState } from "../../app/store";
 
 export interface CartState {
-    items: { [productID: string]: number }
+  items: { [productID: string]: number };
 }
 
 const initialState: CartState = {
-    items: {}
-}
+  items: {},
+};
 
 const cartSlice = createSlice({
-    name: "cart",
-    initialState,
-    reducers: {
-        addToCart(state, action: PayloadAction<string>){
-            const id = action.payload;
-            if(state.items[id]){
-                state.items[id]++
-            } else{
-                state.items[id] = 1
-            }
-        }
-    }
+  name: "cart",
+  initialState,
+  reducers: {
+    addToCart(state, action: PayloadAction<string>) {
+      const id = action.payload;
+      if (state.items[id]) {
+        state.items[id]++;
+      } else {
+        state.items[id] = 1;
+      }
+    },
+    removeFromCart(state, action: PayloadAction<string>) {
+      delete state.items[action.payload];
+    },
+  },
 });
 
-export const { addToCart } = cartSlice.actions;
+export const { addToCart, removeFromCart } = cartSlice.actions;
 export default cartSlice.reducer;
 
-export function getNumItems(state: RootState){
-    let numItems = 0;
-    for(let id in state.cart.items){
-        numItems += state.cart.items[id];
-    }
-    return numItems;
+export function getNumItems(state: RootState) {
+  let numItems = 0;
+  for (let id in state.cart.items) {
+    numItems += state.cart.items[id];
+  }
+  return numItems;
 }
 
 export const getMemoizedNumItems = createSelector(
-    (state: RootState) => state.cart.items,
-    (items) =>{
-        let numItems = 0;
-        for(let id in items){
-            numItems += items[id];
-        }
-        return numItems;
+  (state: RootState) => state.cart.items,
+  (items) => {
+    let numItems = 0;
+    for (let id in items) {
+      numItems += items[id];
     }
-)
+    return numItems;
+  }
+);
 
 export const getTotalPrice = createSelector(
-    (state : RootState)=> state.cart.items,
-    (state : RootState)=> state.products.products,
-    (items, products)=>{
-        let total = 0;
-        for(let id in items){
-            total += products[id].price * items[id];
-        }
-        return total.toFixed(2);
+  (state: RootState) => state.cart.items,
+  (state: RootState) => state.products.products,
+  (items, products) => {
+    let total = 0;
+    for (let id in items) {
+      total += products[id].price * items[id];
     }
-)
+    return total.toFixed(2);
+  }
+);
